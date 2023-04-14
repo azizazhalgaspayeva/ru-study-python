b35a9e16fbfe16a8d6abe6e955faffbcb0ded967
Aziza Zhalgaspayeva <duisembay12@gmail.com>
2023-04-14 22:09:12 +0600

    add maps

--- maps/maps.py
+++ maps/maps.py
@@ -1,4 +1,5 @@
 from typing import Union
+from functools import reduce
 
 
 class MapExercise:
@@ -13,7 +14,15 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+        def map(movie):
+            if ',' in movie['country'] and movie['rating_kinopoisk']:
+                return float(movie['rating_kinopoisk'])    
+            return 0
+
+        ratings = [map(movie) for movie in list_of_movies]
+        ratings = [r for r in ratings if r > 0]
+        avg_rating = reduce(lambda res, x: res + x, ratings, 0) / len(ratings)
+        return avg_rating
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -28,4 +37,11 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+        def map(movie):
+            if movie['rating_kinopoisk'] \
+            and float(movie['rating_kinopoisk']) >= rating:
+                count = [True for char in movie['name'] if char == 'и']
+                return len(count)
+            return 0
+
+        return sum([map(movie) for movie in list_of_movies])
